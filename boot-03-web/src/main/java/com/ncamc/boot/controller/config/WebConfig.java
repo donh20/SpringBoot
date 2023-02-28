d5da74a19b85333f7132599aa517b018bbb1884b
haodong <haodong.donh@gmail.com>
2023-02-28 20:52:53 +0800

    rest 风格的web service

---
+++ boot-03-web/src/main/java/com/ncamc/boot/controller/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ncamc.boot.controller.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration(proxyBeanMethods = false) //容器里放组件没有依赖，所以用false可以快速放
+public class WebConfig {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        hiddenHttpMethodFilter.setMethodParam("_m");
+        return hiddenHttpMethodFilter;
+    }
+}
