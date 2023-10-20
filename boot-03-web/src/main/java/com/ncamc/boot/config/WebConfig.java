435c00e6ad1be6e8b2486c353a47877ed6091d13
haodong <haodong.donh@gmail.com>
2023-10-20 13:40:19 +0800

    重定义自定义内容协商： 1. 基于头的 2. 基于参数的

--- boot-03-web/src/main/java/com/ncamc/boot/config/WebConfig.java
+++ boot-03-web/src/main/java/com/ncamc/boot/config/WebConfig.java
@@ -1,20 +1,26 @@
 package com.ncamc.boot.config;
 
 import com.ncamc.boot.bean.Pet;
+import com.ncamc.boot.converter.NcamcMessageConverter;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration(proxyBeanMethods = false) //容器里放组件没有依赖，所以用false可以快速放
@@ -45,18 +51,37 @@ public class WebConfig /*implements WebMvcConfigurer*/ {
     public WebMvcConfigurer webMvcConfigurer(){
 
         return new WebMvcConfigurer() {
+            /**
+             * 重写configureContentNegotiation
+             * @param configurer
+             */
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                //Map<String, MediaType> mediaTypes
+                Map<String, MediaType> mediaTypes = new HashMap<>();
+                //指定支持解析哪些参数对应的哪些媒体类型
+                mediaTypes.put("json",MediaType.APPLICATION_JSON);
+                mediaTypes.put("xml",MediaType.APPLICATION_XML);
+                mediaTypes.put("ncamc",MediaType.parseMediaType("application/x-ncamc"));
+                ParameterContentNegotiationStrategy parameterContentNegotiationStrategy = new ParameterContentNegotiationStrategy(mediaTypes);
+                HeaderContentNegotiationStrategy headerContentNegotiationStrategy = new HeaderContentNegotiationStrategy();
+                //基于参数的内容协商和基于请求头的内容协商都需要
+                configurer.strategies(Arrays.asList(parameterContentNegotiationStrategy,headerContentNegotiationStrategy));
+                WebMvcConfigurer.super.configureContentNegotiation(configurer);
+            }
 
-//            @Override
-//            @SuppressWarnings("deprecation")
-//            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-//                Map<String, MediaType> mediaTypes = new HashMap<>();
-//                mediaTypes.put("json",MediaType.APPLICATION_JSON);
-//                mediaTypes.put("xml",MediaType.APPLICATION_XML);
-//                mediaTypes.put("ncamc",MediaType.parseMediaType("application/x-ncamc"));
-//                //指定支持解析哪些参数对应的哪些媒体类型
-//
-//
-//            }
+            /**
+             * SpringMVC的任何东西都可以在webMvcConfigurer里修改:
+             * 接口里与MessageConverter有关的接口有两个
+             * 1. configureMessageConverters
+             * 2. extendMessageConverters
+             * 用下面的
+             * @param converters
+             */
+            @Override
+            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+                converters.add(new NcamcMessageConverter());
+            }
 
             @Override
             public void configurePathMatch(PathMatchConfigurer configurer) {
--- boot-03-web/src/main/java/com/ncamc/boot/controller/ResponseTestController.java
+++ boot-03-web/src/main/java/com/ncamc/boot/controller/ResponseTestController.java
@@ -13,14 +13,17 @@ import java.util.Date;
 public class ResponseTestController {
 
     /**
-     * 1. 浏览器发送请求则直接返回 xml application/xml
-     * 2. ajax请求返回json application/json jacksonJsonConverter
-     * 3. 可以自定义协议 ncamc application/ncamc-ia    ncamcDataConverter
+     * 1. 浏览器发送请求则直接返回 xml application/xml jacksonXmlConverter
+     * 2. ajax请求返回json application/json         jacksonJsonConverter
+     * 3. 可以自定义协议 ncamc application/x-ncamc-ia (x表示扩展协议) ncamcDataConverter
      *
      * 步骤：
-     * 添加自定义的MessageConverter
-     * 统计出所有MessageConverter能操作哪些类型
-     * 内容自动协商
+     * 添加自定义的MessageConverter进系统底层
+     * 系统底层自动统计出所有MessageConverter能操作哪些类型
+     * 进行客户端和服务器的内容自动协商【x-ncamc-ia->ncamcDataConverter】
+     * 打开WebMVCAutoConfiguration
+     * org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter#configureMessageConverters(java.util.List)
+     * 把getConverters获取的内容全都加载到容器里
      *
      * @return
      */
