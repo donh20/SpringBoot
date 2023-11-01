fb5118c86bc829a012f57e458527eb9580e09707
haodong <haodong.donh@gmail.com>
2023-11-01 13:58:19 +0800

    动态获取Advanced page数据 通过迭代变量获取当前记录信息stats.count

--- boot-04-admin/src/main/java/com/ncamc/admin/controller/TableController.java
+++ boot-04-admin/src/main/java/com/ncamc/admin/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.ncamc.admin.controller;
 
+import com.ncamc.admin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TableController {
@@ -11,7 +16,14 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model){
+        //遍历表格内容
+        List<User> users = Arrays.asList(new User("zhangsan", "123456"),
+                new User("lisi", "123456"),
+                new User("haha", "aaaaa"),
+                new User("hehe", "bbbbb"));
+        model.addAttribute("users", users);
+        //model.addAttribute(users);
         return "table/dynamic_table";
     }
 
