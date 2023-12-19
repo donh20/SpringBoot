9edc4285861dd9428784fe4feeb275814f4ba3c3
haodong <haodong.donh@gmail.com>
2023-12-19 20:46:12 +0800

    web原生组件注入

--- boot-04-admin/src/main/java/com/ncamc/admin/Boot04AdminApplication.java
+++ boot-04-admin/src/main/java/com/ncamc/admin/Boot04AdminApplication.java
@@ -2,7 +2,9 @@ package com.ncamc.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.ncamc.admin")
 @SpringBootApplication
 public class Boot04AdminApplication {
 
