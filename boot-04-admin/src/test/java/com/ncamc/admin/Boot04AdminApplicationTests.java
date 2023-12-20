d2cd715857e8cf6af5450328e3bdf803d8ed2de5
Dong Hao <haodong.donh@gmail.com>
2023-12-21 00:14:49 +0800

    数据库基本开发场景引入

--- boot-04-admin/src/test/java/com/ncamc/admin/Boot04AdminApplicationTests.java
+++ boot-04-admin/src/test/java/com/ncamc/admin/Boot04AdminApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ncamc.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class Boot04AdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from t_emp", Long.class);
+        log.info("记录总数{}",aLong);
     }
 
 }
