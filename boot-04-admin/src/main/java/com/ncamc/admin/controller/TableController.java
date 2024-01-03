6aad1dfd52f7c305a9c43e5a1491b22734a74f5e
Dong Hao <haodong.donh@gmail.com>
2024-01-03 22:39:49 +0800

    通过mybatis-plus进行删除

--- boot-04-admin/src/main/java/com/ncamc/admin/controller/TableController.java
+++ boot-04-admin/src/main/java/com/ncamc/admin/controller/TableController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +25,17 @@ public class TableController {
         return "table/basic_table";
     }
 
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "pn",defaultValue = "1") Integer pn,
+                             RedirectAttributes ra){
+
+        userService.removeById(id);
+        //重定向属性,添加一个pn
+        ra.addAttribute("pn",pn);
+        return "redirect:/dynamic_table";
+    }
     @GetMapping("/dynamic_table")
     public String dynamic_table(@RequestParam(value="pn",defaultValue = "1") Integer pn, Model model){
         //遍历表格内容
