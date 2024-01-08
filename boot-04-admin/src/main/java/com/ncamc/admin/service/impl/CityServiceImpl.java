abb207ac2d581a1a1ba80e0ad61c1d2120765232
Dong Hao <haodong.donh@gmail.com>
2024-01-08 23:52:10 +0800

    定制actuator信息

--- boot-04-admin/src/main/java/com/ncamc/admin/service/impl/CityServiceImpl.java
+++ boot-04-admin/src/main/java/com/ncamc/admin/service/impl/CityServiceImpl.java
@@ -3,6 +3,8 @@ package com.ncamc.admin.service.impl;
 import com.ncamc.admin.bean.City;
 import com.ncamc.admin.mapper.CityMapper;
 import com.ncamc.admin.service.CityService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,17 @@ public class CityServiceImpl implements CityService {
     @Autowired
     CityMapper cityMapper;
 
+    Counter counter;
+    public CityServiceImpl(MeterRegistry meterRegistry){
+        counter = meterRegistry.counter("cityService.saveCity.count");
+    }
+
     public City getById(Long id){
         return cityMapper.getById(id);
     }
 
     public void saveCity(City city) {
+        counter.increment();
         cityMapper.insertCity(city);
     }
 }
