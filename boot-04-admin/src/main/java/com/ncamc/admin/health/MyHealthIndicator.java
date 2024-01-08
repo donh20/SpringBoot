1fccbb8277aea7e1c857e26e8633a8bf74281eef
Dong Hao <haodong.donh@gmail.com>
2024-01-08 23:13:47 +0800

    定制健康检查信息

---
+++ boot-04-admin/src/main/java/com/ncamc/admin/health/MyHealthIndicator.java
@@ -0,0 +1,32 @@
+package com.ncamc.admin.health;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MyHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        //获取连接进行测试
+        Map<String,Object> map = new HashMap<>();
+        if(1 == 2) {
+//            builder.up(); //健康
+            builder.status(Status.UP);
+            map.put("count",1);
+            map.put("ms",100);
+        } else {
+//            builder.down();//不健康
+            builder.status(Status.OUT_OF_SERVICE);
+            map.put("error","连接超时");
+            map.put("ms",3000);
+        }
+        builder.withDetail("code",100)
+                .withDetails(map);
+
+    }
+}
